53d4c5dbcd2982f349f6883d9b7876b022b01e81
luotuoqingshan <2019hyf@gmail.com>
2024-06-02 21:55:51 -0400

    update comments

--- gen_test.jl
+++ gen_test.jl
@@ -1,10 +1,10 @@
 # modify following keywords for your need
-graphs = ["G$i" for i = 1:10]
-seed = 0
-R = 10
-tol = 0.01
-problem = "LovaszTheta"
-
+graphs = ["G$i" for i = 1:10] # list of graphs to benchmark
+seed = 0 # random seed
+R = 10 # sketch size
+tol = 0.01 # tolerance for primal infeasiblity and suboptimality(relative)
+problem = "MinimumBisection" # problem you want to solve
+path2folder = "~/" # path to the folder SketchyCGAL/
 
 ##
 open(homedir()*"/SketchyCGAL/test_$problem.txt", "w") do io
@@ -12,7 +12,7 @@ open(homedir()*"/SketchyCGAL/test_$problem.txt", "w") do io
         # make sure we warmup
         println(io, "ulimit -d $((16 * 1024 * 1024));"* 
         "matlab -singleCompThread -batch \"cd ~/SketchyCGAL/;"* 
-        "Test_$(problem)_CGAL_PD('G1', $seed, $R, $tol);"*
-        "Test_$(problem)_CGAL_PD('$graph', $seed, $R, $tol);\"")
+        "Test_$(problem)_CGAL_PD('G1', $seed, $R, $tol, '$path2folder');"*
+        "Test_$(problem)_CGAL_PD('$graph', $seed, $R, $tol, '$path2folder');\"")
     end
 end
